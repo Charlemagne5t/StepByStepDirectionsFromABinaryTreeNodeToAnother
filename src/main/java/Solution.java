044bb2831ef5ea311340cb5ab02719603daa7567
alek <wait82985@gmail.com>
2024-07-16 12:56:16 +0500

    prblem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,89 +1,49 @@
 public class Solution {
-    String result = null;
-    public String getDirections(TreeNode root, int startValue, int destValue) {
-        String res = dfs(root, startValue, destValue);
-        //System.out.println(result);
 
-        return result;
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        String[] res = new String[2];
+        dfs(root, startValue, destValue, new StringBuilder(), res);
+
+        int i = 0;
+        int j = 0;
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            if(i < res[0].length() && j < res[1].length() && res[0].charAt(i) == res[1].charAt(j)) {
+                i++;
+                j++;
+                continue;
+            }
+            while(i < res[0].length()) {
+                sb.append("U");
+                i++;
+            }
+            while(j < res[1].length()) {
+                sb.append(res[1].charAt(j));
+                j++;
+            }
+            break;
+        }
+        return sb.toString();
     }
 
-    String dfs(TreeNode root, int s, int d) {
-        if(result != null){
-            return null;
+    void dfs(TreeNode root, int s, int d, StringBuilder path, String[] res) {
+        if(res[0] != null && res[1] != null) {
+            return;
         }
-        if (root == null) {
-            return "-";
+        if(root == null) {
+            return;
         }
-
-        if (root.val == s) {
-            String path1 = "L" + dfs(root.left, s, d);
-            if (path1.charAt(path1.length() - 1) != '-') {
-                path1 = path1.substring(0, path1.length() - 1);
-                if(result == null){
-                    result = path1;
-                }
-                return path1;
-            }
-            String path2 = "R" + dfs(root.right, s, d);
-            if (path2.charAt(path2.length() - 1)  != '-') {
-                path2 = path2.substring(0,path2.length() - 1);
-                if(result == null){
-                    result = path2;
-                }
-                return path2;
-            }
-
-            return "S";
+        if(root.val == s) {
+            res[0] = path.toString();
         }
-        if (root.val == d) {
-            String path1 = "U" + dfs(root.left, s, d);
-            if (path1.charAt(path1.length() - 1) != '-') {
-                path1 = "U".repeat(path1.length() - 1);
-                if(result == null){
-                    result = path1;
-                }
-                return path1;
-            }
-            String path2 = "U" + dfs(root.right, s, d);
-            if (path2.charAt(path2.length() - 1)  != '-') {
-                path2 = "U".repeat(path2.length() - 1);
-                if(result == null){
-                    result = path2;
-                }
-                return path2;
-            }
-
-            return "D";
+        if(root.val == d) {
+            res[1] = path.toString();
         }
-        String left = "L" + dfs(root.left, s, d);
-        String right = "R" + dfs(root.right, s, d);
-
-        if (left.charAt(left.length() - 1) == '-') {
-            return right;
-        }
-        if (right.charAt(right.length() - 1) == '-') {
-            return left;
-        }
-
-        if(left.charAt(left.length() - 1) == 'S') {
-            int len = left.length();
-            StringBuilder sb = new StringBuilder();
-            sb.append("U".repeat(len - 1));
-            if(result == null){
-                result = sb + right.substring(0, right.length() - 1);
-            }
-            return sb + right.substring(0, right.length() - 1);
-        }
-        if(right.charAt(right.length() - 1) == 'S') {
-            int len = right.length();
-            StringBuilder sb = new StringBuilder();
-            sb.append("U".repeat(len - 1));
-            if(result == null){
-                result = sb + left.substring(0, left.length() - 1);
-            }
-            return sb + left.substring(0, left.length() - 1);
-        }
-
-        return "-";
+        path.append("L");
+        dfs(root.left, s, d, path, res);
+        path.deleteCharAt(path.length() - 1);
+        path.append("R");
+        dfs(root.right, s, d, path, res);
+        path.deleteCharAt(path.length() - 1);
     }
 }
